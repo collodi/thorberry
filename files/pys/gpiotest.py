82700c078b97a46808280369ae6fb9b08c663518
laptop <wkim149@gmail.com>
2016-05-19 03:56:32 +0000

    output test done

--- files/pys/gpiotest.py
+++ files/pys/gpiotest.py
@@ -6,12 +6,10 @@ if __name__ == '__main__':
 	gpio = ThorGPIO()
         face = ThorFace()
 	states = [0, 1, 2, 3, 2, 1, 0]
-	index = 0
 	try:
-		while True:
+		for index in states:
 			gpio.update(states[index])
                         face.update(states[index])
-			index = (index + 1) % len(states)
                         time.sleep(5)
 	except:
 		gpio.close()
--- index.js
+++ index.js
@@ -138,7 +138,8 @@ app.all('*', function(req, res, next) {
 
 // output test
 app.get('/lightest', function(req, res, next) {
-    spawn('python', ['/home/pi/www/files/pys/gpiotest.py'], { stdio: 'ignore', detached: true });
+    	var sid = req.cookies.sessionid;
+	spawn('python', ['/home/pi/www/files/pys/gpiotest.py'], { stdio: 'ignore', detached: true });
 
 	res.to = '/';
 	res.msg = '5 seconds for each stage of warning. Redirecting you to the main page.';
