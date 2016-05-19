276ece7fac9c01999b937c93ee234d3cdd5169f7
laptop <wkim149@gmail.com>
2016-05-19 02:24:45 +0000

    gpio test extended to PiFace

--- files/pys/gpiotest.py
+++ files/pys/gpiotest.py
@@ -1,13 +1,17 @@
 from thorgpio import ThorGPIO
+from thorface import ThorFace
 
 if __name__ == '__main__':
 	gpio = ThorGPIO()
+        face = ThorFace()
 	states = [0, 1, 2, 3, 2, 1, 0]
 	index = 0
 	try:
 		while True:
 			raw_input('Waiting Input... ' + str(states[index]))
 			gpio.update(states[index])
+                        face.update(states[index])
 			index = (index + 1) % len(states)
 	except:
 		gpio.close()
+                face.close()
