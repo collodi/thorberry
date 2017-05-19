f046d0970d98834588f6adcf5ea1182c0734749e
pinocchio <wkim149@gmail.com>
2017-05-18 23:37:04 -0400

    add redirect

--- test/controllers/pins_controller_test.rb
+++ test/controllers/pins_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class PinsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get pins_index_url
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 
--- test/controllers/status_controller_test.rb
+++ test/controllers/status_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class StatusControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get status_index_url
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 
